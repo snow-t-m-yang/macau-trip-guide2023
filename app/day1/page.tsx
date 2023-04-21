13d4216cf6e95a84cebf76fa36e7b30454207913
Snow Yang <ytm199891@gmail.com>
2023-04-21 13:23:36 +0800

    feat: Add calendar bar. Add CheckList.

--- app/day1/page.tsx
+++ app/day1/page.tsx
@@ -1,4 +1,81 @@
-const page = () => {
-  return <div>page</div>;
+"use client";
+import { useState, useEffect } from "react";
+import { v4 as uuidv4 } from "uuid";
+
+type CheckListType = {
+  id: string;
+  item: string;
 };
+
+const CheckListSource: CheckListType[] = [
+  {
+    item: "護照",
+    id: uuidv4(),
+  },
+  {
+    item: "台胞證",
+    id: uuidv4(),
+  },
+  {
+    item: "充電器",
+    id: uuidv4(),
+  },
+];
+
+const page = () => {
+  const [checkedItem, setCheckedItem] = useState(0);
+  const [allChecked, setAllChecked] = useState(false);
+
+  const handleIsChecked = (checked: Boolean) => {
+    if (checked) {
+      setCheckedItem((i) => i + 1);
+    } else {
+      setCheckedItem((i) => i - 1);
+    }
+  };
+
+  useEffect(() => {
+    if (checkedItem === CheckListSource.length) {
+      setAllChecked(true);
+    }
+  }, [checkedItem]);
+
+  return (
+    <section className="grid min-h-[100dvh] place-items-center">
+      <h1 className="text-5xl">Day 1</h1>
+
+      {/* CheckList */}
+      {!allChecked && (
+        <div className="border-t-2 border-secondary bg-accent shadow-3xl rounded-xl">
+          <table
+            className={`table-accent rounded-xl border-separate border-spacing-x-12 border-spacing-y-5`}
+          >
+            <thead>
+              <tr>
+                <th className="text-3xl ">最後檢查</th>
+              </tr>
+            </thead>
+            <tbody>
+              {CheckListSource.map((listItem) => (
+                <tr key={listItem.id}>
+                  <th>
+                    <label className="flex items-center gap-5">
+                      <input
+                        type="checkbox"
+                        onChange={(e) => handleIsChecked(e.target.checked)}
+                        className="border-white shadow-inner checkbox"
+                      />
+                      <div className="text-2xl font-bold ">{listItem.item}</div>
+                    </label>
+                  </th>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )}
+    </section>
+  );
+};
+
 export default page;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,22 @@
+"use client";
+
 import "./globals.css";
 import Link from "next/link";
+import {
+  AiOutlineHome,
+  AiOutlineInfoCircle,
+  AiFillInfoCircle,
+  AiFillHome,
+  AiOutlineCalendar,
+  AiFillCalendar,
+} from "react-icons/ai";
 
-export const metadata = {
-  title: "Mocau and HonKong trip 2023",
-  description: "A website guide for Macau and HongKong trip 2023",
+import { useState, useReducer } from "react";
+
+const navbarStates = {
+  home: true,
+  info: false,
+  calendar: false,
 };
 
 export default function RootLayout({
@@ -11,87 +24,77 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  type NavbarState = {
+    home: boolean;
+    info: boolean;
+    calendar: boolean;
+  };
+
+  type NavbarAction = {
+    type: string;
+  };
+
+  const reducer = (state: NavbarState, action: NavbarAction) => {
+    switch (action.type) {
+      case "home":
+        return { home: true, info: false, calendar: false };
+      case "info":
+        return { home: false, info: true, calendar: false };
+      case "calendar":
+        return { home: false, info: false, calendar: true };
+      default:
+        throw new Error();
+    }
+  };
+
+  const [active, setActive] = useState(false);
+  const [state, dispatch] = useReducer(reducer, navbarStates);
+
   return (
     <html lang="en" className="bg-primary">
       <link rel="icon" href="/favicon.ico" type="image/x-icon" sizes="any" />
       <body className="max-w-3xl mx-auto text-white">
         {children}
         <div className="fixed inset-x-0 bottom-0 flex w-full mx-auto">
-          <ul className="flex justify-center w-full menu bg-white/20 backdrop-blur-3xl menu-horizontal">
-            <Link href="/info">
-              <li>
-                <a>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="w-5 h-5"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                    />
-                  </svg>
-                </a>
-              </li>
-            </Link>
-
-            <Link href="/">
-              <li>
-                <a>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="w-5 h-5"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
-                    />
-                  </svg>
-                </a>
-              </li>
-            </Link>
-            <li>
-              <a>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="w-5 h-5"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
-                  />
-                </svg>
-              </a>
+          <ul className="flex justify-center w-full gap-5 py-3 text-2xl bg-white/20 backdrop-blur-3xl menu-horizontal">
+            <li onClick={() => dispatch({ type: "info" })}>
+              <Link href="/info">
+                {state.info ? <AiFillInfoCircle /> : <AiOutlineInfoCircle />}
+              </Link>
             </li>
+
+            <li onClick={() => dispatch({ type: "home" })}>
+              <Link href="/">
+                {state.home ? <AiFillHome /> : <AiOutlineHome />}
+              </Link>
+            </li>
+
+            <li onClick={() => dispatch({ type: "calendar" })}>
+              {state.calendar ? <AiFillCalendar /> : <AiOutlineCalendar />}
+            </li>
+            <ul
+              className={`${
+                state.calendar ? "right-0" : "right-[-100%]"
+              } group absolute w-full bg-success text-base-100 grid grid-cols-5 p-3  bottom-[100%] duration-500 divide-x divide-black/20`}
+            >
+              <li>
+                <a href="/day1">Day1</a>
+              </li>
+              <li>
+                <a href="/day2">Day2</a>
+              </li>
+              <li>
+                <a href="/day3">Day3</a>
+              </li>
+              <li>
+                <a href="/day4">Day4</a>
+              </li>
+              <li>
+                <a href="/day5">Day5</a>
+              </li>
+            </ul>
           </ul>
         </div>
-        {/* <ul
-          className={` group absolute bottom-0 w-56 menu bg-base-100 rounded-box`}
-        >
-          <li>
-            <a>Item 1</a>
-          </li>
-          <li>
-            <a>Item 2</a>
-          </li>
-          <li>
-            <a>Item 3</a>
-          </li>
-        </ul> */}
 
         <footer className="p-10 bg-primary footer footer-center text-base-content">
           <div className="grid grid-flow-col gap-4">
