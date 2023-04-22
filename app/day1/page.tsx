5df6c474adc38affde613bb9c7cb3ad85efd5069
Snow Yang <ytm199891@gmail.com>
2023-04-22 15:07:38 +0800

    bugfix: Adjust checklist

--- app/day1/page.tsx
+++ app/day1/page.tsx
@@ -5,75 +5,85 @@ import { v4 as uuidv4 } from "uuid";
 type CheckListType = {
   id: string;
   item: string;
+  checked: boolean;
 };
 
 const CheckListSource: CheckListType[] = [
   {
     item: "護照",
     id: uuidv4(),
+    checked: false,
   },
   {
     item: "臺胞證",
     id: uuidv4(),
+    checked: false,
   },
   {
     item: "充電器",
     id: uuidv4(),
+    checked: false,
   },
 ];
 
 const page = () => {
-  const [checkedItem, setCheckedItem] = useState(0);
-  const [allChecked, setAllChecked] = useState(false);
+  const [checkedList, setCheckedList] =
+    useState<CheckListType[]>(CheckListSource);
 
-  const handleIsChecked = (checked: Boolean) => {
-    if (checked) {
-      setCheckedItem((i) => i + 1);
-    } else {
-      setCheckedItem((i) => i - 1);
-    }
+  const handleIsChecked = (checked: boolean, id: string) => {
+    console.log(checked);
+    const updatedList = checkedList.map((item) => {
+      if (item.id === id) {
+        return { ...item, checked: !checked };
+      }
+      return item;
+    });
+
+    setCheckedList(updatedList);
   };
 
-  useEffect(() => {
-    if (checkedItem === CheckListSource.length) {
-      setAllChecked(true);
-    }
-  }, [checkedItem]);
+  // useEffect(() => {
+  //   localStorage.setItem("checkedList_state", JSON.stringify(checkedList));
+  // }, [checkedList]);
+
+  // useEffect(() => {
+  //   const checkedListData = localStorage.getItem("checkedList_state");
+  //   if (checkedListData !== null) {
+  //     setCheckedList(JSON.parse(checkedListData));
+  //   }
+  // }, []);
 
   return (
     <section className="grid w-full text-center px-3 py-3 gap-5 min-h-[100dvh] ">
       {/* CheckList */}
-      {!allChecked && (
-        <div>
-          <h2 className="text-3xl">最後檢查</h2>
-          <div className="py-5 bg-white/20 shadow-3xl rounded-xl">
-            <table
-              className={`table-accent rounded-xl border-separate border-spacing-x-12 border-spacing-y-2 mx-auto`}
-            >
-              <tbody className="flex flex-col justify-center w-full ">
-                {CheckListSource.map((listItem) => (
-                  <tr key={listItem.id}>
-                    <th>
-                      <label className="flex items-center gap-5">
-                        <input
-                          type="checkbox"
-                          onChange={(e) => handleIsChecked(e.target.checked)}
-                          className="border-white shadow-inner checkbox"
-                        />
-                        <div className="text-2xl font-bold ">
-                          {listItem.item}
-                        </div>
-                      </label>
-                    </th>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
-        </div>
-      )}
 
-      <div>
+      <div className="space-y-3">
+        <h2 className="text-3xl">最後檢查</h2>
+        <div className="py-5 bg-white/20 shadow-3xl rounded-xl">
+          <ul className="flex flex-col">
+            {checkedList.map((listItem) => (
+              <li
+                key={listItem.id}
+                className="self-start mx-auto justify-self-start"
+              >
+                <label className="flex gap-5">
+                  <input
+                    type="checkbox"
+                    checked={listItem.checked}
+                    onChange={(e) =>
+                      handleIsChecked(e.target.checked, listItem.id)
+                    }
+                    className="border-white shadow-inner checkbox"
+                  />
+                  <div className="text-2xl font-bold ">{listItem.item}</div>
+                </label>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+
+      <div className="space-y-3">
         <h2 className="text-3xl ">航班資訊</h2>
         <iframe
           className="w-full h-56 rounded-2xl"
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,111 +1,22 @@
 "use client";
 
 import "./globals.css";
-import Link from "next/link";
-import {
-  AiOutlineHome,
-  AiOutlineInfoCircle,
-  AiFillInfoCircle,
-  AiFillHome,
-  AiOutlineCalendar,
-  AiFillCalendar,
-} from "react-icons/ai";
 
-import { useState, useReducer } from "react";
-
-const navbarStates = {
-  home: true,
-  info: false,
-  calendar: false,
-};
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  type NavbarState = {
-    home: boolean;
-    info: boolean;
-    calendar: boolean;
-  };
-
-  type NavbarAction = {
-    type: string;
-  };
-
-  const reducer = (state: NavbarState, action: NavbarAction) => {
-    switch (action.type) {
-      case "home":
-        return { home: true, info: false, calendar: false };
-      case "info":
-        return { home: false, info: true, calendar: false };
-      case "calendar":
-        return { home: false, info: false, calendar: true };
-      default:
-        throw new Error();
-    }
-  };
-
-  const [active, setActive] = useState(false);
-  const [state, dispatch] = useReducer(reducer, navbarStates);
-
   return (
     <html lang="en" className="bg-primary">
       <link rel="icon" href="/favicon.ico" type="image/x-icon" sizes="any" />
       <body className="max-w-3xl mx-auto text-white">
-        {children}
-        <div className="fixed inset-x-0 bottom-0 flex w-full mx-auto">
-          <ul className="flex justify-center w-full gap-5 py-3 text-3xl bg-white/20 backdrop-blur-3xl menu-horizontal">
-            <li onClick={() => dispatch({ type: "info" })}>
-              <Link href="/info">
-                {state.info ? <AiFillInfoCircle /> : <AiOutlineInfoCircle />}
-              </Link>
-            </li>
-
-            <li onClick={() => dispatch({ type: "home" })}>
-              <Link href="/">
-                {state.home ? <AiFillHome /> : <AiOutlineHome />}
-              </Link>
-            </li>
-
-            <li onClick={() => dispatch({ type: "calendar" })}>
-              {state.calendar ? <AiFillCalendar /> : <AiOutlineCalendar />}
-            </li>
-            <ul
-              className={`${
-                state.calendar ? "right-0" : "right-[-100%]"
-              } absolute w-full bg-white/20 text-white flex justify-between p-3  bottom-[100%] duration-500  divide-black/20 text-xl `}
-            >
-              <li>
-                <a href="/day1">Day1</a>
-              </li>
-              <li>
-                <a href="/day2">Day2</a>
-              </li>
-              <li>
-                <a href="/day3">Day3</a>
-              </li>
-              <li>
-                <a href="/day4">Day4</a>
-              </li>
-              <li>
-                <a href="/day5">Day5</a>
-              </li>
-            </ul>
-          </ul>
-        </div>
-
-        <footer className="p-10 mt-10 bg-primary footer footer-center text-base-content">
-          <div className="grid grid-flow-col gap-4">
-            <a className="link link-hover">航班資訊</a>
-            <a className="link link-hover">飯店資訊</a>
-            <a className="link link-hover">聯繫方式</a>
-          </div>
-          <div>
-            <p>Copyright © 2023 - All right reserved by Snow Yang</p>
-          </div>
-        </footer>
+        <main className="max-w-md mx-auto">{children}</main>
+        <Navbar />
+        <Footer />
       </body>
     </html>
   );
