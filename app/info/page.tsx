c368a8c65ca2889489c95f75ee11c5eef7a27152
Snow Yang <ytm199891@gmail.com>
2023-05-20 10:45:13 +0800

    feat: Update next 13.4

--- app/info/page.tsx
+++ app/info/page.tsx
@@ -5,7 +5,7 @@ const Infopage = () => {
         id="air"
         className="min-h-[50dvh]  flex flex-col  items-center gap-2"
       >
-        <h1 className="p-3 text-secondary text-2xl">航班資訊</h1>
+        <h1 className="p-3 text-2xl text-secondary">航班資訊</h1>
         <div className=" grid grid-cols-2     h-[20dvh] w-[90%]   rounded-3xl bg-white/20">
           <h2 className=" flex items-start w-[50%] justify-self-center col-start-1 col-end-2 row-start-1 row-end-2 text-secondary  pt-3 text-lg ">
             去程：{" "}
@@ -13,43 +13,43 @@ const Infopage = () => {
           <p className=" col-start-1 col-end-2 row-start-2 row-end-3 justify-self-center  flex items-start w-[50%]">
             航班號
           </p>
-          <p className="px-3 col-start-2 col-end-3 row-start-2 row-end-3 justify-self-start">
+          <p className="col-start-2 col-end-3 row-start-2 row-end-3 px-3 justify-self-start">
             NX615 0527
           </p>
           <p className=" col-start-1 col-end-2 row-start-3 row-end-4 justify-self-center  flex items-start w-[50%]">
             時間
           </p>
-          <p className=" px-3 col-start-2 col-end-3 row-start-3 row-end-4 justify-self-start">
+          <p className="col-start-2 col-end-3 row-start-3 row-end-4 px-3 justify-self-start">
             20:05 - 22:00
           </p>
           <p className=" col-start-1 col-end-2 row-start-4 row-end-5 justify-self-center  flex items-start w-[50%]">
             地點
           </p>
-          <p className="px-3 col-start-2 col-end-3 row-start-4 row-end-5 justify-self-start">
+          <p className="col-start-2 col-end-3 row-start-4 row-end-5 px-3 justify-self-start">
             {" "}
             台北桃園T1 - 澳門
           </p>
         </div>
-        <div className=" grid grid-cols-2 justify-center     h-[20dvh] w-[90%]   rounded-3xl bg-white/20">
+        <div className=" grid grid-cols-2 justify-center h-[20dvh] w-[90%]   rounded-3xl bg-white/20">
           <h2 className=" justify-self-center text-lg col-start-1 col-end-2 row-start-1 row-end-2 text-secondary pt-3  w-[50%] items-start ">
             回程：{" "}
           </h2>
           <p className=" col-start-1 col-end-2 row-start-2 row-end-3 justify-self-center flex items-start w-[50%] ">
             航班號
           </p>
-          <p className="px-3 col-start-2 col-end-3 row-start-2 row-end-3 justify-self-start">
+          <p className="col-start-2 col-end-3 row-start-2 row-end-3 px-3 justify-self-start">
             NX618 0531
           </p>
           <p className=" col-start-1 col-end-2 row-start-3 row-end-4 flex items-start justify-self-center w-[50%]">
             時間
           </p>
-          <p className="px-3 col-start-2 col-end-3 row-start-3 row-end-4 justify-self-start">
+          <p className="col-start-2 col-end-3 row-start-3 row-end-4 px-3 justify-self-start">
             17:35 - 19:15
           </p>
           <p className=" col-start-1 col-end-2 row-start-4 row-end-5 flex items-start justify-self-center w-[50%]">
             地點
           </p>
-          <p className="px-3 col-start-2 col-end-3 row-start-4 row-end-5 justify-self-start">
+          <p className="col-start-2 col-end-3 row-start-4 row-end-5 px-3 justify-self-start">
             {" "}
             澳門 - 台北桃園T1
           </p>
